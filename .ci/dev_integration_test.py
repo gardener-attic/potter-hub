64ff5ea1eee0e921c6b6d34cc9f2cf5b822eb5a4
Jens Huebel <j.huebel@sap.com>
2021-01-05 16:17:26 +0100

    ci: change integration test

--- .ci/dev_integration_test.py
+++ .ci/dev_integration_test.py
@@ -8,6 +8,8 @@ hub_path = os.environ['SOURCE_PATH']
 
 os.environ['HUB_PATH'] = os.environ['SOURCE_PATH']
 os.environ['ROOT_PATH'] = root_path
+os.environ['LANDSCAPE'] = "dev"
+os.environ['NAMESPACE'] = "release-test"
 
 print("Run integration test on dev")
 
