bb2ae2085f06f92dcf7f703bc5ef39677ea1ccf5
Guenther Wannenmacher <guenther.wannenmacher@sap.com>
2021-05-18 12:23:52 +0200

    update nginx to v1.20.0

--- .ci/utils.py
+++ .ci/utils.py
@@ -173,7 +173,7 @@ def replace_chart_placeholder(chart_path: str, image_version: str, chart_version
 
     replace_placeholder_in_yaml(values_yaml, "<NGINX_REGISTRY>", "eu.gcr.io")
     replace_placeholder_in_yaml(values_yaml, "<NGINX_REPOSITORY>", image_repo_3rd + "nginx")
-    replace_placeholder_in_yaml(values_yaml, "<NGINX_TAG>", "1.19.10-alpine")
+    replace_placeholder_in_yaml(values_yaml, "<NGINX_TAG>", "1.20.0-alpine")
 
     replace_placeholder_in_yaml(values_yaml, "<IMAGE_PULL_POLICY>", "IfNotPresent")
 
